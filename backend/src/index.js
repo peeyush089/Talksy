d2d6aa5340402d432e375b0d07ae27c57e30f052
Peeyush Yadav <peeyushyadav089@gmail.com>
2026-05-18 17:11:07 +0530

    fix cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,10 @@ const allowedOrigins = [
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({
+  origin: "https://talksy-taupe.vercel.app", 
+  credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
